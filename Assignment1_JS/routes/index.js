8c593ade04c758d896649dceefdc685655e5b310
Thales Valente <46799430+Duifas@users.noreply.github.com>
2020-06-28 19:42:14 -0400

    Creation of all the pages

---
+++ Assignment1_JS/routes/index.js
@@ -0,0 +1,27 @@
+﻿'use strict';
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/about', function (req, res) {
+    res.render('about', { title: 'Express' });
+});
+
+router.get('/contact', function (req, res) {
+    res.render('contact', { title: 'Express' });
+});
+
+router.get('/projects', function (req, res) {
+    res.render('projects', { title: 'Express' });
+});
+
+router.get('/services', function (req, res) {
+    res.render('services', { title: 'Express' });
+});
+
+
+module.exports = router;
